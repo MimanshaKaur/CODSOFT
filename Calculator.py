8abe1164823d76ee5b2aa74954daf28521138764
Mimansha Kaur <134724715+MimanshaKaur@users.noreply.github.com>
2024-03-10 22:11:45 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,36 @@
-choice=0
+from tkinter import *
+
+#creating screen
+screen=Tk()
+screen.title("CALCULATOR")
+screen.geometry("450x550")
+screen.configure(background="black")
+
+def number():
+    n=9
+    return n
+
+#creating calculator
+input_text=StringVar()
+
+lf=LabelFrame(screen)
+lf.pack(pady=20)
+
+result=Entry(lf,text='',font=("cursive",20),background="darkgray")
+result.pack(pady=20,padx=20)
+
+button_frame=Frame(screen,width=350,height=380,bg="red")
+button_frame.pack()
+
+button9=Button(lf,text=9,fg="white",bg="black",command= number)
+
+
+
+
+
+
+
+'''choice=0
 while(choice==0):
     a=int(input("Enter First Number: "))
     b=int(input("Enter Second Number: "))
@@ -23,4 +55,5 @@ while(choice==0):
     if(c=="YES" or c=="yes"):
         choice=0
     else:
-        choice=1
+        choice=1'''
+screen.mainloop()
